d2a54fb82ca33c4fd050c58b433a1df0d50d0f92
Chris Jennings <chrisjennings555@gmail.com>
2019-04-04 14:48:24 +0100

    person and program

--- udemyApp/Person.cs
+++ udemyApp/Person.cs
@@ -4,10 +4,17 @@ namespace udemyApp
 {
     class Person
     {
-        
-        private String name { get; set; }
-        private int age { get; set; }
-        private  String nickname { get; set; }
+        public String name { get; set; }
+        public int age { get; set; }
+        public  String nickname { get; set; }
+
+          public Person(String name, int age, String nickname)
+        {
+            this.name = name;
+            this.age = age;
+            this.nickname = nickname;
+        }
+
 
     }
 }
--- udemyApp/Program.cs
+++ udemyApp/Program.cs
@@ -6,9 +6,9 @@ namespace udemyApp
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person();
+            Person person1 = new Person("Chris", 30, "Legend");
 
-            
+            Console.WriteLine(person1);
         }
     }
 }
