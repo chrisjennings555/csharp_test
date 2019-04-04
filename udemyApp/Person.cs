ea550255f49ab72445086711f9362daf5928c9a7
Chris Jennings <chrisjennings555@gmail.com>
2019-04-04 14:11:12 +0100

    saved file

---
+++ udemyApp/Person.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace udemyApp
+{
+    class Person
+    {
+        
+        private String name { get; set; }
+        private int age { get; set; }
+        private  String nickname { get; set; }
+
+    }
+}
+
--- udemyApp/Program.cs
+++ udemyApp/Program.cs
@@ -6,21 +6,7 @@ namespace udemyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, please tell me your name...");
-            string username = Console.ReadLine();
-
-            switch (username)
-            {
-                case "Chris":
-                Console.WriteLine("Hello Chris");
-                break;
-                case "Kate":
-                Console.WriteLine("You have your own laptop");
-                break;
-                default:
-                Console.WriteLine("INTRUDER");
-                break;
-            }
+            Person person1 = new Person();
 
             
         }
