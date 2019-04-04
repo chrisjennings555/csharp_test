b4fdd164043b5bbbdebe069493c6d0858d8a7c70
Chris Jennings <chrisjennings555@gmail.com>
2019-04-04 11:13:36 +0100

    square number

--- udemyApp/Program.cs
+++ udemyApp/Program.cs
@@ -11,14 +11,18 @@ namespace udemyApp
             double anotherNumber = 11.5;
             bool isTrue = true;
             string input = Console.ReadLine();
+            int convertInputToNumber;
+            int.TryParse(input, out convertInputToNumber);
+            int squareInput = convertInputToNumber * convertInputToNumber;
             var anything = 11.6;
 
             Console.WriteLine(someText);
             Console.WriteLine(someNumber);
             Console.WriteLine(anotherNumber);
             Console.WriteLine(isTrue);
-            Console.WriteLine(input);
+            Console.WriteLine("Inputted number = " + input);
             Console.WriteLine(anything);
+            Console.WriteLine(squareInput);
         }
     }
 }
